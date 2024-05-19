5c76c7c60ee6e6c809a85cbe7e6f3f88186e5da4
holakirr <kpetunin@icloud.com>
2024-05-18 19:56:50 -0600

    chore: Update footer links to open in new tab

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,11 +11,19 @@ export const Footer = () => {
 			<div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
 				<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
 					© {yearRange} Developed by{" "}
-					<a href="https://holakirr.com/" className="hover:underline">
+					<a
+						href="https://holakirr.com/"
+						target="blank"
+						className="hover:underline"
+					>
 						Kirill Petunin
 					</a>{" "}
 					for{" "}
-					<a href="https://alex-fomin.vercel.app/" className="hover:underline">
+					<a
+						href="https://alex-fomin.vercel.app/"
+						target="blank"
+						className="hover:underline"
+					>
 						Alexander Fomin
 					</a>
 					. All Rights Reserved.
